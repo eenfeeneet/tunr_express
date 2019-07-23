cec39d9ca425bf9aa67eeba7c599a002ce29d2e3
2Burn <burnhafiz@gmail.com>
2019-07-24 05:06:17 +0800

    first commit.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 console.log("starting up!!");
 
+/**
+ * ===================================
+ * Configurations and set up
+ * ===================================
+ */
+
 const express = require('express');
 const methodOverride = require('method-override');
 const pg = require('pg');
 
 // Initialise postgres client
 const configs = {
-  user: 'YOURUSERNAME',
-  host: '127.0.0.1',
-  database: 'tunr_db',
-  port: 5432,
+    user: 'mhafiz',
+    host: '127.0.0.1',
+    database: 'tunr_db',
+    password:'popo25',
+    port: 5432
 };
 
 const pool = new pg.Pool(configs);
@@ -18,11 +25,7 @@ pool.on('error', function (err) {
   console.log('idle client error', err.message, err.stack);
 });
 
-/**
- * ===================================
- * Configurations and set up
- * ===================================
- */
+
 
 // Init express app
 const app = express();
@@ -48,11 +51,20 @@ app.engine('jsx', reactEngine);
  * ===================================
  */
 
-app.get('/', (request, response) => {
+app.get('/', (req, res) => {
   // query database for all pokemon
+  let queryText = 'SELECT * FROM items ORDER BY id ASC';
+  pool.query(queryText, (err, result) => {
+    if (err) {
+      console.error('query error:', err.stack);
+      response.send( 'query error' );
+    } else {
+      console.log('query result:', result);
 
-  // respond with HTML page displaying all pokemon
-  response.render('home');
+
+      // respond with HTML page displaying all pokemon
+      response.render('home');
+  }
 });
 
 app.get('/new', (request, response) => {
@@ -66,17 +78,15 @@ app.get('/new', (request, response) => {
  * Listen to requests on port 3000
  * ===================================
  */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const port = 3000;
+const server = app.listen(port, () => console.log(`~~~ Tuning in to the waves of port ${port} ~~~`));
 
 let onClose = function(){
-  
-  console.log("closing");
-  
-  server.close(() => {
-    
-    console.log('Process terminated');
-    
-    pool.end( () => console.log('Shut down db connection pool'));
+    console.log("closing");
+    server.close(() => {
+        console.log('Process terminated');
+        pool.end( () =>
+            console.log('Shut down db connection pool'));
   })
 };
 
